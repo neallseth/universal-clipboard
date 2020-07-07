9e5df8457b1ebe4ba2bf6225fcfd31684e4976ec
Neall Seth <nseth@vt.edu>
2020-07-06 22:16:53 -0400

    seting up db connection

--- pages/api/clip.js
+++ pages/api/clip.js
@@ -1,6 +1,52 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { Client } = require("pg");
+
+const clientConfig = {
+  user: "jezchcpq",
+  host: "drona.db.elephantsql.com",
+  database: "jezchcpq",
+  password: "Itq0PC8O3DZVIC6dxdEcZDai8dkC5LkT",
+  port: 5432,
+};
 
 export default async (req, res) => {
+  const client = new Client(clientConfig);
+
+  let response;
+  if (req.method === "POST") {
+    console.log("post received with the body: ", req.body);
+  } else if (req.query.id) {
+    response = await getEntryByID(client, req.query.id);
+  } else {
+    response = await getAllRecords(client);
+  }
   res.statusCode = 200;
-  res.json({ requestedID: req.query.id });
+  res.json(response);
 };
+
+async function getAllRecords(client) {
+  await client.connect();
+  const queryRes = await client.query("SELECT * FROM clips");
+  await client.end();
+
+  return queryRes.rows;
+}
+
+async function getEntryByID(client, id) {
+  await client.connect();
+
+  const sql = "SELECT id,clip_entry FROM clips WHERE id = $1";
+  const params = [id];
+
+  console.log("id: ", id);
+
+  try {
+    const res = await client.query(sql, params);
+    console.log(res.rows);
+    await client.end();
+    return res.rows[0];
+  } catch (err) {
+    await client.end();
+    console.log(err.stack);
+    return err.stack;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -9,107 +9,67 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">Universal Clipboard</h1>
+        <h1 className="title">Universal Clipboard 📋</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <p className="description">Paste here 📍 | Access anywhere 🌌</p>
+        <div className="input-container">
+          <textarea className="text-input" spellCheck="false"></textarea>
         </div>
+        <button>Get Link</button>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
+        .input-container {
+          width: 100%;
+          height: 25rem;
+          margin-top: 3rem;
+        }
+
+        .text-input {
+          width: 100%;
+          height: 100%;
+          border-radius: 50px;
+          background: #538bf3;
+          box-shadow: 20px 20px 60px #4776cf, -20px -20px 60px #5fa0ff;
+          font-size: 2rem;
+          padding: 2.75rem;
+          line-height: 1.25;
+          color: black;
+          border: none;
+          transition: all 0.15s ease-in;
+          outline: none;
+          resize: none;
+          -webkit-appearance: none;
+        }
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
+          padding: 5rem 2rem;
+          background: #538bf3;
           display: flex;
-          flex-direction: column;
           justify-content: center;
-          align-items: center;
-          background-color: orange;
+        }
+
+        button {
+          border-radius: 0.4rem;
+          height: 3rem;
+          width: 100%;
+          max-width: 15rem;
+          border: none;
+          background-color: #7caaff;
+          cursor: pointer;
+          transition: all 0.15s ease-in;
+          letter-spacing: 0.1em;
+          margin-top: 4rem;
+          outline: none;
+          font-size: 1.25rem;
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
           align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
+          max-width: 50rem;
         }
 
         .title {
@@ -128,79 +88,24 @@ export default function Home() {
           font-size: 1.5rem;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+        @media (max-width: 415px) {
+          .title {
+            font-size: 3rem;
+          }
+          .container {
+            padding: 2rem 2rem;
           }
         }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+        @media (max-height: 740px) {
+          .input-container {
+            margin-top: 0rem;
+          }
         }
 
-        * {
-          box-sizing: border-box;
+        @media (max-height: 670px) {
+          .input-container {
+            height: 20rem;
+          }
         }
       `}</style>
     </div>
