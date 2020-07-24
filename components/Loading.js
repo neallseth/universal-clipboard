635c2ff70422497f2c31d2cc545bea1e3d7d53a2
Neall Seth <nseth@vt.edu>
2020-07-24 15:23:42 -0400

    tweaking loading animation

--- components/Loading.js
+++ components/Loading.js
@@ -4,11 +4,11 @@ export default function Loading() {
       <style jsx>
         {`
           .loader {
-            border: 1.25rem solid #002474;
+            border: 0.5rem solid #0024746b;
             border-radius: 50%;
-            border-top: 1.25rem solid #88b2ff;
-            width: 10rem;
-            height: 10rem;
+            border-top: 0.5rem solid #88b2ff;
+            width: 8rem;
+            height: 8rem;
             animation: spin 0.35s linear infinite;
           }
 
--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home() {
               }}
               value={userEntry}
             ></textarea>
-          )}{" "}
+          )}
         </div>
 
         <div className="btnContainer">
